d2a09626279bdde4ea32b76c22737e0242dddd40
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-03-06 20:50:28 +0100

    Initial commit

---
+++ time_analysis.py
@@ -0,0 +1,131 @@
+"""
+Investigate how long synchronization between individual cells takes
+"""
+
+import io
+
+import numpy as np
+import networkx as nx
+
+import matplotlib.pylab as plt
+import matplotlib as mpl
+import matplotlib.image as mpimg
+
+from utils import solve_system
+
+
+def plot_matrix(mat, ax):
+    """ Plot system evolution
+    """
+    im = ax.imshow(mat, interpolation='nearest')
+    plt.colorbar(im, ax=ax)
+
+    ax.set_xlabel(r'$i$')
+    ax.set_ylabel(r'$j$')
+
+    ax.set_title('Synchronization duration')
+
+def plot_graph(graph, ax):
+    """ Plot graph
+    """
+    pydot_graph = nx.nx_pydot.to_pydot(graph)
+    png_str = pydot_graph.create_png(prog=['dot', '-Gdpi=300'])
+    img = mpimg.imread(io.BytesIO(png_str))
+
+    ax.imshow(img, aspect='equal')
+    ax.axis('off')
+
+def plot_evolutions(sols, ts, ax):
+    """ Plot system evolution
+    """
+    for i, sol in enumerate(sols):
+        ax.plot(ts, sol, label=r'$f_{{{}}}$'.format(i))
+
+    ax.set_xlabel(r'$t$')
+    ax.set_ylabel(r'$\Theta_i$')
+
+    ax.set_ylim((0, 2*np.pi))
+    ax.legend(loc='best')
+
+def plot_result(graph, mat, evo):
+    """ Plot final result
+    """
+    fig = plt.figure(figsize=(30, 10))
+    gs = mpl.gridspec.GridSpec(1, 3, width_ratios=[1, 1, 2])
+
+    plot_graph(graph, plt.subplot(gs[0]))
+    plot_matrix(mat, plt.subplot(gs[1]))
+    plot_evolutions(*evo, plt.subplot(gs[2]))
+
+    fig.savefig('result.pdf', dpi=300)
+    fig.savefig('foo.png')
+
+def generate_sync_matrix(sols):
+    """ Generate matrix with synchronization time in each cell
+    """
+    max_len = len(sols[0])
+
+    mat = []
+    for sol in sols:
+        mat.append([])
+        for osol in sols:
+            offset = 0
+            diff = abs(sol[offset:] - osol[offset:])
+            ind = None
+
+            for i, e in enumerate(diff):
+                if np.isclose(e, 0, atol=0.1):
+                    ind = i
+                    break
+
+            if ind is None:
+                ind = max_len
+
+            dur = ind / max_len
+            mat[-1].append(dur)
+
+    return np.array(mat)
+
+def generate_graph(size):
+    """ Generate graph with clusters
+    """
+    clus_num = 4
+    graph = nx.complete_graph(size)
+
+    for j in range(1, clus_num):
+        g = nx.complete_graph(size)
+        nl_map = {}
+        for i in range(size):
+            nl_map[i] = i + size * j
+        g = nx.relabel_nodes(g, nl_map)
+
+        graph = nx.compose(graph, g)
+
+    graph.add_edges_from([
+        (0,4), (1,5),
+        (8,12), (9,13),
+        (0,12)
+    ])
+
+    return graph
+
+def simulate_system(size):
+    """ Show system evolution for different adjacency matrices
+    """
+    graph = generate_graph(size)
+
+    adjacency_matrix = nx.to_numpy_matrix(graph)
+    omega_vec = np.ones((len(graph.nodes()),)) * 0.3
+
+    sols, ts = solve_system(omega_vec, adjacency_matrix)
+    mat = generate_sync_matrix(sols.T)
+
+    plot_result(graph, mat, (sols.T, ts))
+
+def main():
+    """ General interface
+    """
+    simulate_system(4)
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,36 @@
+"""
+Commonly used functions
+"""
+
+import numpy as np
+import numpy.random as npr
+
+from scipy.integrate import odeint
+
+
+def generate_system(omega_vec, A):
+    """ Generate generalized Kuramoto model
+    """
+    def func(state, t=0):
+        ode = []
+        for i, o in enumerate(omega_vec):
+            ode.append(
+                o + np.sum(
+                    [A[i,j] * np.sin(state[j] - state[i])
+                        for j in range(len(omega_vec))])
+            )
+        return np.array(ode)
+    return func
+
+def solve_system(O, A, tmax=20, dt=0.01):
+    """ Solve particular configuration
+    """
+    func = generate_system(O, A)
+
+    ts = np.arange(0, tmax, dt)
+    init = npr.uniform(0, 2*np.pi, size=O.shape)
+
+    sol = odeint(func, init, ts)
+    sol %= 2*np.pi
+
+    return sol, ts
